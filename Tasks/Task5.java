3916cbe57a8505fe9fb869308d89e783dcf84055
Boranbayev Danial <1.dani.qwerty@gmail.com>
2025-03-30 22:25:22 +0500

    Task 5

--- Tasks/Task5.java
+++ Tasks/Task5.java
@@ -1,4 +1,18 @@
 package Tasks;
 
+import java.util.Scanner;
+
 public class Task5 {
+    // Time Complexity: O(2^n) - Exponential recursion
+    public static int findFibonacci(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        return findFibonacci(n - 1) + findFibonacci(n - 2);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(findFibonacci(n));
+    }
 }
