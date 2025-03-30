21572f8227bf40e5b2672b99130fe31851f42039
Boranbayev Danial <1.dani.qwerty@gmail.com>
2025-03-30 22:28:35 +0500

    Task 7

--- Tasks/Task7.java
+++ Tasks/Task7.java
@@ -1,4 +1,29 @@
 package Tasks;
 
+import java.util.Scanner;
+
 public class Task7 {
+    // Time Complexity: O(n) - Reversing elements using recursion
+    public static void reverse(int[] arr, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        reverse(arr, start + 1, end - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        reverse(arr, 0, n - 1);
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
 }
