634fff70885244db733939dd8d0e3d85da457abd
Boranbayev Danial <1.dani.qwerty@gmail.com>
2025-03-30 22:31:16 +0500

    Task 10

--- Tasks/Task10.java
+++ Tasks/Task10.java
@@ -1,4 +1,20 @@
 package Tasks;
 
+import java.util.Scanner;
+
 public class Task10 {
+    // Time Complexity: O(log(min(a, b))) - Euclidean Algorithm
+    public static int findGCD(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return findGCD(b, a % b);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(findGCD(a, b));
+    }
 }
