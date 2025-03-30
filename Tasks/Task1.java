6f667422892945d859e542eec5083dc058a1a5f6
Boranbayev Danial <1.dani.qwerty@gmail.com>
2025-03-30 22:15:40 +0500

    Task 1

--- Tasks/Task1.java
+++ Tasks/Task1.java
@@ -1,4 +1,25 @@
 package Tasks;
 
+import java.util.Scanner;
+
 public class Task1 {
+    public static int findMin(int[] arr, int n) {
+        int min = arr[0];
+        for (int i = 1; i < n; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(findMin(arr, n));
+    }
 }
