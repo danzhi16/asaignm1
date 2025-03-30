cd7b90782c76baf55836eee7dd51caf8267d5a75
Boranbayev Danial <1.dani.qwerty@gmail.com>
2025-03-30 22:26:32 +0500

    Task 6

--- Tasks/Task6.java
+++ Tasks/Task6.java
@@ -1,4 +1,20 @@
 package Tasks;
 
+import java.util.Scanner;
+
 public class Task6 {
+    // Time Complexity: O(n) - Linear recursion
+    public static int findPower(int a, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        return a * findPower(a, n - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int n = sc.nextInt();
+        System.out.println(findPower(a, n));
+    }
 }
