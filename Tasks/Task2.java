9663207ef3a2be851ec43c18a7df6b5af6ed1f12
Boranbayev Danial <1.dani.qwerty@gmail.com>
2025-03-30 22:17:57 +0500

    Task 2

--- Tasks/Task2.java
+++ Tasks/Task2.java
@@ -1,4 +1,23 @@
 package Tasks;
 
+import java.util.Scanner;
+
 public class Task2 {
+    public static double findAverage(int[] arr, int n) {
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+        }
+        return (double) sum / n;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(findAverage(arr, n));
+    }
 }
