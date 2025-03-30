5f08e0a97cbbcd2e25a179c27268f72e9f4d6154
Boranbayev Danial <1.dani.qwerty@gmail.com>
2025-03-30 22:30:10 +0500

    Task 8

--- Tasks/Task9.java
+++ Tasks/Task9.java
@@ -1,4 +1,20 @@
 package Tasks;
 
+import java.util.Scanner;
+
 public class Task9 {
+    // Time Complexity: O(2^n) - Exponential recursion
+    public static int findBinomial(int n, int k) {
+        if (k == 0 || k == n) {
+            return 1;
+        }
+        return findBinomial(n - 1, k - 1) + findBinomial(n - 1, k);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        System.out.println(findBinomial(n, k));
+    }
 }
